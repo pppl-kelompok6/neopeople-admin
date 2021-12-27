f9a8ceb6b41032676de68b566fc92fe198269d94
Ridho Muhammad <50231796+Lacutee@users.noreply.github.com>
2021-12-27 21:07:40 +0700

    add calender

--- pages/counseling/index.js
+++ pages/counseling/index.js
@@ -1,10 +1,19 @@
+import Calender from '../../src/components/konselingProps/calender'
+import ListCounseler from '../../src/components/konselingProps/ListCounseler'
 import Layout from '../../src/components/MainLayout'
 
 export default function Counseling() {
   return (
-    <div className='flex flex-cols'>
-      <Layout/>
-      <p>Counseling</p>
-    </div>
+      <Layout>
+          <div className="h-fit lg:grid grid-cols-3 md:grid-rows-2 gap-8 p-8">
+            <div className="">
+              <ListCounseler/>
+            </div>
+            <div className="col-span-2">
+              <Calender/>
+            </div>
+              
+          </div>
+      </Layout>
   )
 }
